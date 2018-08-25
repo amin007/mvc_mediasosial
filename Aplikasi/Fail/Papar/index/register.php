7e7e1967d562fb354bd6dfa593285de8f7c5b018
amirul laptop asus <mohamadamiruljamari@gmail.com>
2018-08-25 21:34:53 +0800

    ubah nama medan

--- Aplikasi/Fail/Papar/index/register.php
+++ Aplikasi/Fail/Papar/index/register.php
@@ -7,29 +7,35 @@
                     <div class="col-md-offset-7 col-lg-offset-7 col-md-5 col-lg-5 col-sm-12 col-xs-12">
                         <div class="contact-form mb50 wow fadeIn">
                             <h2>Register</h2>
-                            <form action="process.php" id="contact-form" method="post">
-                                <div class="form-group" id="name-field">
+                            <form action="<?php echo URL . 'register/username' ?>" method="POST">
+                                <div class="form-group" id="username-field">
                                     <div class="form-input">
-                                        <input type="text" class="form-control" id="form-name" name="form-name" placeholder="Name.." required>
+                                        <input type="text" class="form-control" id="form-name" name="users[0][pengguna]" placeholder="Username..." required>
                                     </div>
                                 </div>
                                 <div class="form-group" id="email-field">
                                     <div class="form-input">
-                                        <input type="email" class="form-control" id="form-email" name="form-email" placeholder="Email.." required>
+                                        <input type="email" class="form-control" id="form-email" name="users[0][email]" placeholder="Email.." required>
                                     </div>
                                 </div>
-                                <div class="form-group" id="phone-field">
+                                <div class="form-group" id="password-field">
                                     <div class="form-input">
-                                        <input type="text" class="form-control" id="form-phone" name="form-phone" placeholder="Subject..">
+                                        <input type="password" class="form-control" id="form-phone" name="users[0][katalaluan1]" placeholder="Password...">
                                     </div>
                                 </div>
-                                <div class="form-group" id="message-field">
+								 <div class="form-group" id="password-field">
                                     <div class="form-input">
-                                        <textarea class="form-control" rows="6" id="form-message" name="form-message" placeholder="Your Message Here..." required></textarea>
+                                        <input type="password" class="form-control" id="form-phone" name="users[0][katalaluan2]" placeholder="Re-enter Password...">
                                     </div>
                                 </div>
-                                <div class="form-group">
-                                    <button type="submit">Register</button>
+								<div class="form-group" id="phone-field">
+                                    <div class="form-input">
+                                        <input type="text" class="form-control" id="form-phone" name="users[0][notelefon]" placeholder="Phone number...">
+                                    </div>
+                                </div
+                                
+                                <div class="form-group" >
+									<input type="submit" class="form-control" value="Register">
                                 </div>
                             </form>
                         </div>
