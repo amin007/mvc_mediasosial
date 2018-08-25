ef2c910eaadbb5592659e3da9ed78c5202feabc5
amirul laptop asus <mohamadamiruljamari@gmail.com>
2018-08-25 17:51:09 +0800

    isi template login

--- Aplikasi/Fail/Papar/index/login.php
+++ Aplikasi/Fail/Papar/index/login.php
@@ -1,4 +1,32 @@
-<?php
-
-$paparkan = new \Aplikasi\Kitab\Bacafail;
-$paparkan->login();
+ <!--CONTACT US AREA-->
+    <section class="contact-area relative padding-100-50" id="contact">
+        <div class="area-bg"></div>
+        <div class="contact-form-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-offset-7 col-lg-offset-7 col-md-5 col-lg-5 col-sm-12 col-xs-12">
+                        <div class="contact-form mb50 wow fadeIn">
+                            <h2>LOGIN</h2>
+                            <form action="process.php" id="contact-form" method="post">
+                                <div class="form-group" id="email-field">
+                                    <div class="form-input">
+                                        <input type="email" class="form-control" id="form-name" name="form-name" placeholder="Email..." required>
+                                    </div>
+                                </div>
+                                <div class="form-group" id="password-field">
+                                    <div class="form-input">
+                                        <input type="password" class="form-control" id="form-email" name="form-email" placeholder="Password.." required>
+                                    </div>
+                                </div>
+                                
+                                <div class="form-group">
+                                    <button type="submit">LOGIN</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--CONTACT US AREA END-->
