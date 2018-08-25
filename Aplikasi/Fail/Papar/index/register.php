54d3d7632b87e191a72d3fea331e793ee537a71b
amirul laptop asus <mohamadamiruljamari@gmail.com>
2018-08-25 17:51:40 +0800

    tambah template register

---
+++ Aplikasi/Fail/Papar/index/register.php
@@ -0,0 +1,41 @@
+ <!--CONTACT US AREA-->
+    <section class="contact-area relative padding-100-50" id="contact">
+        <div class="area-bg"></div>
+        <div class="contact-form-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-offset-7 col-lg-offset-7 col-md-5 col-lg-5 col-sm-12 col-xs-12">
+                        <div class="contact-form mb50 wow fadeIn">
+                            <h2>Register</h2>
+                            <form action="process.php" id="contact-form" method="post">
+                                <div class="form-group" id="name-field">
+                                    <div class="form-input">
+                                        <input type="text" class="form-control" id="form-name" name="form-name" placeholder="Name.." required>
+                                    </div>
+                                </div>
+                                <div class="form-group" id="email-field">
+                                    <div class="form-input">
+                                        <input type="email" class="form-control" id="form-email" name="form-email" placeholder="Email.." required>
+                                    </div>
+                                </div>
+                                <div class="form-group" id="phone-field">
+                                    <div class="form-input">
+                                        <input type="text" class="form-control" id="form-phone" name="form-phone" placeholder="Subject..">
+                                    </div>
+                                </div>
+                                <div class="form-group" id="message-field">
+                                    <div class="form-input">
+                                        <textarea class="form-control" rows="6" id="form-message" name="form-message" placeholder="Your Message Here..." required></textarea>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit">Register</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--CONTACT US AREA END-->
